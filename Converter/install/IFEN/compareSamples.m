447c470ef73d810f0f8928c8a520d59b460b9c15
JamesTCurran <jamestcurran@gmail.com>
2015-12-17 13:31:29 +0100

    Fixed IFENSetx

--- Converter/install/IFEN/compareSamples.m
+++ Converter/install/IFEN/compareSamples.m
@@ -1,23 +1,28 @@
 clear all;
-
+close all;
 
 fpt=fopen( 'RoofTop_FE0_Band0.stream', 'rb' );
 sampt=fread( fpt, 1000, 'bit2' );
 fclose( fpt );
-
 sampt=2*sampt+1;
-
 
 fp=fopen( 'L1.dat', 'rb' );
 samp = fread( fp, 1000, 'int8' );
 fclose( fp );
 
 
+plot( sampt, samp, '.-b' );
+hold on; grid on;
 
 
-plot( sampt, samp );
+fpt=fopen( 'RoofTop_FE0_Band1.stream', 'rb' );
+sampt=fread( fpt, 1000, 'bit2' );
+fclose( fpt );
+sampt=2*sampt+1;
 
-for i=1:4:12
-    sampt(i:i+3)'
-    samp(i:i+3)'
-end
+fp=fopen( 'L2.dat', 'rb' );
+samp = fread( fp, 1000, 'int8' );
+fclose( fp );
+
+
+plot( sampt, samp, '.-r' );
